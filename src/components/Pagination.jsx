c4feb6f8cce722706f5d5c7b431ad04eb4130281
anujs1430 <anujs1430@gmail.com>
2024-09-02 11:18:49 +0530

    second commit- GOTOPAGE directly by input

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,80 +1,118 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Pagination = () => {
+  const center = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+  };
 
-    const center = {
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column'
-    }
+  const api = "https://dummyjson.com/users?limit=0";
 
-    const api = 'https://dummyjson.com/users?limit=0';
+  const [tableData, setTableData] = useState([]);
+  const [currPage, setCurrPage] = useState(1);
+  const [rowPerPage, setRowPerPage] = useState(10);
 
-    const [tableData, setTableData] = useState([]);
-    const [currPage, setCurrPage] = useState(1);
-    const [rowPerPage, setRowPerPage] = useState(10);
+  const [inputVal, setInputVal] = useState(1);
 
-    const indexOfLastItem = currPage * rowPerPage;
-    const indexOfFirstItem = indexOfLastItem - rowPerPage;
-    const currItems = tableData?.slice(indexOfFirstItem, indexOfLastItem);
+  const indexOfLastItem = currPage * rowPerPage;
+  const indexOfFirstItem = indexOfLastItem - rowPerPage;
+  const currItems = tableData?.slice(indexOfFirstItem, indexOfLastItem);
 
-    const totalBtn = tableData.length > 0 ? Math.ceil(tableData.length / rowPerPage) : 0;
+  const totalBtn =
+    tableData.length > 0 ? Math.ceil(tableData.length / rowPerPage) : 0;
 
-    useEffect(() => {
-        axios.get(api).then((res) => {
-            console.log('totalBtn', totalBtn);
-            setTableData(res.data.users);
+  useEffect(() => {
+    axios
+      .get(api)
+      .then((res) => {
+        console.log("totalBtn", totalBtn);
+        setTableData(res.data.users);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }, []);
 
-        }).catch((error) => {
-            console.log("error", error);
-        });
-    }, []);
+  const gotoPage = () => {
+    console.log("click");
+    setCurrPage(inputVal);
+  };
 
-    return (
-        <div style={center}>
-            <table border={1}>
-                <thead>
-                    <tr>
-                        <th>S No.</th>
-                        <th>Full Name</th>
-                        <th>Email</th>
-                        <th>Gender</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {currItems?.map((item, index) => (
-                        <tr key={index}>
-                            <td>{indexOfFirstItem + index + 1}</td>
-                            <td>{item.firstName} {item.lastName}</td>
-                            <td>{item.email}</td>
-                            <td>{item.gender}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div>
-                <button
-                    onClick={() => setCurrPage(currPage - 1)}
-                    disabled={currPage === 1}
-                >Prev</button>
+  const nextBtn = () => {
+    setCurrPage(currPage + 1);
+    setInputVal(currPage + 1);
+  };
 
-                {totalBtn > 0 && Array.from({ length: totalBtn }, (_, index) => (
-                    <button
-                        key={index + 1}
-                        onClick={() => setCurrPage(index + 1)}
-                        disabled={currPage === index + 1}
-                    >
-                        {index + 1}
-                    </button>
-                ))}
-                <button
-                    onClick={() => setCurrPage(currPage + 1)}
-                    disabled={currPage == totalBtn}
-                >Next</button>
-            </div>
+  const prevBtn = () => {
+    setCurrPage(currPage - 1);
+    setInputVal(currPage - 1);
+  };
+
+  const totalPagesBtn = (index) => {
+    setCurrPage(index + 1);
+    setInputVal(index + 1);
+  };
+
+  return (
+    <div style={center}>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>S No.</th>
+            <th>Full Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currItems?.map((item, index) => (
+            <tr key={index}>
+              <td>{indexOfFirstItem + index + 1}</td>
+              <td>
+                {item.firstName} {item.lastName}
+              </td>
+              <td>{item.email}</td>
+              <td>{item.gender}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="table-bottom">
+        <div>
+          <p>{`Showing page no ${currPage} of total ${totalBtn}`}</p>
+          <input
+            type="number"
+            value={inputVal}
+            onChange={(e) => setInputVal(e.target.value)}
+          />
+          <button type="submit" onClick={gotoPage}>
+            Go
+          </button>
         </div>
-    )
-}
 
-export default Pagination
+        <div className="pagination">
+          <button onClick={prevBtn} disabled={currPage === 1}>
+            Prev
+          </button>
+          <button onClick={nextBtn} disabled={currPage == totalBtn}>
+            Next
+          </button>
+          {totalBtn > 0 &&
+            Array.from({ length: totalBtn }, (_, index) => (
+              <button
+                key={index + 1}
+                onClick={() => totalPagesBtn(index)}
+                disabled={currPage === index + 1}
+              >
+                {index + 1}
+              </button>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
